3eb4c5b3b92dab78f74787bf8f2d830809d123b2
Daniel Pena <163460072+daperk@users.noreply.github.com>
2024-09-28 19:39:56 +0000

    header

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   return( 
   <>
     <header>
-      <a>
-        
-      </a>
+      <a href="" >Tico Tico</a>
+      <nav>
+        <Link href={''} >Home</Link>
+        <Link href={''} >Menu</Link>
+        <Link href={''} >About</Link>
+        <Link href={''} >Contact</Link>
+        <Link href={''} >Login</Link>
+      </nav>
     </header>
   </>
   )
